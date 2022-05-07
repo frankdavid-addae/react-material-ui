5ea01fc97cd08b2bd102499a9345f3e6694b7436
Frank David Addae <f.davidaddae@korba365.com>
2022-05-07 18:51:46 +0000

    added data table

--- src/pages/storage/Storage.jsx
+++ src/pages/storage/Storage.jsx
@@ -1,16 +1,11 @@
-import React, { useState, useEffect } from "react";
-import CustomButton from "../../components/common/custom_button/CustomButton";
+import React, { useState } from "react";
+import CustomCard from "../../components/common/custom_card/CustomCard";
 import CustomSnackbar from "../../components/common/custom_snackbar/custom_snackbar";
 import GridWrapper from "../../components/common/grid_wrapper/GridWrapper";
-import Loading from "../../components/common/loading/Loading";
+import UserTable from "../../components/user_table/UserTable";
 
 const Storage = () => {
   const [open, setOpen] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  const handleClick = () => {
-    setOpen(true);
-  };
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
@@ -20,32 +15,17 @@ const Storage = () => {
     setOpen(false);
   };
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <GridWrapper>
-      {loading ? (
-        <Loading>
-          <CustomButton variant="contained" onClick={handleClick}>
-            Open success snackbar
-          </CustomButton>
-        </Loading>
-      ) : (
-        <CustomButton variant="contained" onClick={handleClick}>
-          Open success snackbar
-        </CustomButton>
-      )}
-      <CustomSnackbar
-        open={open}
-        onClose={handleClose}
-        severity="error"
-        message="Error message"
-      />
+      <CustomCard 
+          content={<UserTable onError={() => setOpen(true)} />}
+        />
+        <CustomSnackbar
+          open={open}
+          severity="error"
+          message="Data couldn't be fetched"
+          onClose={handleClose}
+        />
     </GridWrapper>
   );
 };
